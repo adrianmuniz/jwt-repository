fe5187b91ba71483198f83320cb69b0e41390215
Cainã Muniz <adrianmuniz.ceo@outlook.com>
2025-03-26 00:46:04 -0300

    Refatorando m/login com tratamento de exceção

--- src/main/java/com/dev/token/jwt/controllers/AuthenticationController.java
+++ src/main/java/com/dev/token/jwt/controllers/AuthenticationController.java
@@ -8,11 +8,13 @@ import com.dev.token.jwt.models.User;
 import com.dev.token.jwt.security.JwtProvider;
 import com.dev.token.jwt.services.UserService;
 import jakarta.validation.Valid;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -20,6 +22,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Log4j2
 @RestController
 @CrossOrigin(origins = "x", maxAge = 3600)
 @RequestMapping("/auth")
@@ -43,12 +46,16 @@ public class AuthenticationController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<JwtDto> autenticarUser(@Valid @RequestBody LoginDto loginDto){
-        Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword()));
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        String jwt = jwtProvider.gerarToken(authentication);
-        return ResponseEntity.ok(new JwtDto(jwt));
+    public ResponseEntity<?> autenticarUser(@Valid @RequestBody LoginDto loginDto){
+        try {
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword()));
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            String jwt = jwtProvider.gerarToken(authentication);
+            return ResponseEntity.ok(new JwtDto(jwt));
+        } catch (BadCredentialsException e){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Usuario ou senha incorretos!");
+        }
     }
 
     @GetMapping("/usuarios")
